ab06ee0bf544303a99a06fe885df38424f466c12
Rickard <rickard80@gmail.com>
2025-02-21 12:07:00 +0100

    Updated python with a scheduler

--- prismat/storecrawler.py
+++ prismat/storecrawler.py
@@ -1,10 +1,29 @@
 from urllib.request import urlopen
-import re
-import json
+import re, json
+import sched, time
 
+# Sets a year+dayOfYear integer at every successful update
+updatedStores = {
+  "Willys": 0
+}
 
+s = sched.scheduler(time.time, time.sleep)
+
+getTodayAsInt = lambda : int(time.strftime("%Y%j", time.gmtime()))
+isMonday = lambda : int(time.strftime("%u")) == 1
+needsUpdate = lambda storeName : getTodayAsInt() != updatedStores[storeName]
+
+# Run a method only once during a Monday
+def onlyOnMondays(storeName, callback):
+  if isMonday() & needsUpdate(storeName):
+    callback()
+
+  runSchedulerService()
+
+# Opens, writes and closes a json file
 def storeWillysDiscountAPIUrl():
   parameter = getWillysDiscountParameter()
+
   apiUrl = f'https://www.willys.se/productBannerComponent/{parameter}?size=999'
   filename = 'stores.json'
 
@@ -24,17 +43,19 @@ def storeWillysDiscountAPIUrl():
 
     storesFile.close()
 
+    print("👍 Updated Willys api url")
 
+    updatedStores['Willys'] = getTodayAsInt()
+
+# Parsing the Willy's homepage for the parameter containing discounts
 def getWillysDiscountParameter():
   discountKeyword = '\"Veckans varor\"'[::-1] # reversing
   parameterKeyword = '\"uid\"'[::-1]  # reversing
-
-  #regexpDiscount = f'{discountKeyword}.+?{parameterKeyword}' # greedy
-  regexpDiscount = f'{discountKeyword}[\s\S]+?{parameterKeyword}'
+  regexpDiscount = f'{discountKeyword}.+?{parameterKeyword}' # greedy
   regexpParameter = 'comp_\w+'
   url = 'https://www.willys.se'
 
-  return 'comp_0000M17T'
+  #return 'comp_0000M17T'
 
   with urlopen(url) as response:
     html = response.read().decode('utf-8')
@@ -47,9 +68,16 @@ def getWillysDiscountParameter():
       if matchParameter:
         discountParameter = matchParameter.group()
         print(f'getWillysDiscountParameter: Found parameter: {discountParameter}')
+
+        return discountParameter
       else:
         print("getWillysDiscountParameter: Couldn't match PARAMETER")
     else:
       print("getWillysDiscountParameter: Couldn't match DISCOUNT")
 
-storeWillysDiscountAPIUrl()
+def runSchedulerService():
+  every_hour = 3600
+  s.enter(every_hour, 2, onlyOnMondays, argument=("Willys", storeWillysDiscountAPIUrl))
+  s.run()
+  
+runSchedulerService()
